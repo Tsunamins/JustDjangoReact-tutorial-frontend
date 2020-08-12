7ca9c89b995d94f44e4a250215356f99cfca3c9b
Alison Reilly <reillyamr@gmail.com>
2020-08-12 11:20:11 -0400

    add compDidMount with axios

--- src/containers/ArticleListView.js
+++ src/containers/ArticleListView.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import Article from '../components/Article'
+import Articles from '../components/Article'
+
+const listData = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://ant.design',
+    title: `ant design part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 class ArticleList extends React.Component {
+
+    state = {
+        articles: [],
+
+    }
+
+    // called everytime component mounted
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8000/api/')
+        .then(resp => {
+            this.setState({
+                articles: resp.data
+            });
+            console.log(resp.data);
+
+        } )
+
+    }
+
     render(){
         return(
-            <Article />
+            <Articles data={listData} />
 
         )
     }
 }
+
+export default ArticleList
