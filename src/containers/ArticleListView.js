d20940b79482b4d9e1e41c193fbe46571fd4496e
Alison Reilly <reillyamr@gmail.com>
2020-08-12 12:35:17 -0400

    add fetch equivalent

--- src/containers/ArticleListView.js
+++ src/containers/ArticleListView.js
@@ -25,14 +25,20 @@ class ArticleList extends React.Component {
 
     // called everytime component mounted
     componentDidMount(){
+
         axios.get('http://127.0.0.1:8000/api/')
         .then(resp => {
             this.setState({
                 articles: resp.data
             });
             console.log(resp.data);
-
         } )
+
+        // fetch equivalent:
+        // fetch('http://127.0.0.1:8000/api/')
+        // .then(resp => resp.json())
+        // .then(data => console.log(data))
+
 
     }
 
