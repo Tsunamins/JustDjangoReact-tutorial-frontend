3a34eb4eb1bed02317d1d15be68bf3c809d8e27c
Alison Reilly <reillyamr@gmail.com>
2020-09-18 14:10:36 -0400

    add store and middleware to app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { createStore, compose, applyMiddleware } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducers/auth';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhances(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  
+  <Provider store={store}>
     <App />
+  </Provider>
   ,
   document.getElementById('root')
 );
